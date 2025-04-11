817cf3cd3dc0a140ed44e89378989198db7f8e27
cassianobraz <cassanopb@gmail.com>
2025-04-11 13:47:43 -0400

    feat erros mensagens

--- ProductClientHub.API/Controllers/ClientsController.cs
+++ ProductClientHub.API/Controllers/ClientsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProductClientHub.API.UseCases.Clients.Register;
 using ProductClientHub.Communication.Requests;
 using ProductClientHub.Communication.Responses;
 
@@ -9,9 +10,25 @@ public class ClientsController : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(ResponseClientJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status400BadRequest)]
     public IActionResult Register([FromBody] RequestClientJson request)
     {
-        return Created();
+        try
+        {
+            var useCase = new RegisterClientUseCase();
+
+            var response = useCase.Execute(request);
+
+            return Created(string.Empty, response);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(new ResponseErrorMessagesJson(ex.Message));
+        }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorMessagesJson("UNKNOWN ERRO"));
+        }
     }    
     
     [HttpPut]
