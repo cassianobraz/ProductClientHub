4dd6cde70008bbc23f8e34ee82666c05e0367444
cassianobraz <cassanopb@gmail.com>
2025-04-11 16:29:48 -0400

    feat line

--- ProductClientHub.API/Entities/Product.cs
+++ ProductClientHub.API/Entities/Product.cs
@@ -2,5 +2,9 @@
 
 public class Product
 {
-
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public Guid ClientId { get; set; }
 }
